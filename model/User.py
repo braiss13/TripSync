3319ca8f778275d130702b79be121961e9bea901
braiss13 <bsampayolorenzo@gmail.com>
2024-05-22 19:31:19 +0200

    Trips Functional

--- model/User.py
+++ model/User.py
@@ -3,22 +3,39 @@ import sirope
 import werkzeug.security as safe
 
 class User(flask_login.mixins.UserMixin):
-    def __init__(self, email, pswd):
+    def __init__(self, email, pswd, name, surname, age, phone):
         self.__email = email
         self.__pswd = safe.generate_password_hash(pswd)
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
+        self.__phone = phone
 
     @property
     def email(self):
         return self.__email
-    ...
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @property
+    def age(self):
+        return self.__age
+
+    @property
+    def phone(self):
+        return self.__phone
 
     def get_id(self):
         return self.email
-    ...
 
     def chk_pswd(self, other_pswd):
         return safe.check_password_hash(self.__pswd, other_pswd)
-    ...
 
     @staticmethod
     def current():
@@ -27,13 +44,9 @@ class User(flask_login.mixins.UserMixin):
         if usr.is_anonymous:
             flask_login.logout_user()
             usr = None
-        ...
 
         return usr
-    ...
 
     @staticmethod
     def find(srp: sirope.Sirope, email: str) -> "User":
         return srp.find_first(User, lambda u: u.email == email)
-    ...
-...
--- views/user.py
+++ views/user.py
@@ -1,14 +1,16 @@
 import flask
+import flask_login
 import sirope
 from model.User import User
+from model.Trip import Trip
 
 def get_blprint():
     usr_module = flask.blueprints.Blueprint("user_blpr", __name__,
                                             url_prefix="/user",
                                             template_folder="templates",
                                             static_folder="static")
-    srp = sirope.Sirope()
-    return usr_module, srp
+    syrp = sirope.Sirope()
+    return usr_module, syrp
 
 user_blpr, srp = get_blprint()
 
@@ -16,16 +18,31 @@ user_blpr, srp = get_blprint()
 def user_add():
     usr_email = flask.request.form.get("edEmail", "").strip()
     usr_passw = flask.request.form.get("edPswd", "").strip()
+    usr_name = flask.request.form.get("edName", "").strip()
+    usr_surname = flask.request.form.get("edSurname", "").strip()
+    usr_age = flask.request.form.get("edAge", "").strip()
+    usr_phone = flask.request.form.get("edPhone", "").strip()
 
-    if not usr_email or not usr_passw:
-        flask.flash("Missing credentials...", "danger")
+    if not usr_email or not usr_passw or not usr_name or not usr_surname or not usr_age or not usr_phone:
+        flask.flash("All fields are required.", "danger")
         return flask.redirect("/")
-    
+
     if User.find(srp, usr_email):
-        flask.flash("User already exists!", "danger")
+        flask.flash("User already exists.", "danger")
         return flask.redirect("/")
-    
-    usr = User(usr_email, usr_passw)
+
+    usr = User(usr_email, usr_passw, usr_name, usr_surname, int(usr_age), usr_phone)
     srp.save(usr)
-    flask.flash("You can now log in with your new credentials.", "success")
-    return flask.redirect("/")
+    flask_login.login_user(usr)
+    flask.flash("Registration successful! You are now logged in.", "success")
+
+    # Obtener la lista de viajes para el usuario recién registrado
+    trip_list = list(srp.filter(Trip, lambda t: t.user_id == usr.email))
+    
+    sust = {
+        "usr": usr,
+        "srp": srp,
+        "trip_list": trip_list
+    }
+
+    return flask.render_template("index.html", **sust)
